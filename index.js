3a99374dcd148d9528b294d4c130076047637b96
Aman Chahar <amanchahar1702@gmail.com>
2024-06-16 16:11:53 +0530

    CORS

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 //ROUTES
 app.use("/api/user", userRouter)
